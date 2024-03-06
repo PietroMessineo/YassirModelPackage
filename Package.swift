c6fd93ce7ed2f44f0f527676fd22044f87ff8295
Pietro <pmessineo@purple-technology.com>
2024-03-06 17:16:29 +0100

    Transfered Model classes to Package

--- Package.swift
+++ Package.swift
@@ -5,17 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "ModelPackage",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ModelPackage",
             targets: ["ModelPackage"]),
     ],
+    dependencies: [
+        
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ModelPackage"),
+            name: "ModelPackage",
+            dependencies: [
+                // "ComponentsPackage"
+            ]),
         .testTarget(
             name: "ModelPackageTests",
             dependencies: ["ModelPackage"]),
