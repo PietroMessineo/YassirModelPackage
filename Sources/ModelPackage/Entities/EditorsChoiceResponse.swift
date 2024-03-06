c0f13e393a72ce995d9bcf3bb7f1ac85793cbc63
Pietro <pmessineo@purple-technology.com>
2024-03-06 18:12:35 +0100

    Added public init

--- Sources/ModelPackage/Entities/EditorsChoiceResponse.swift
+++ Sources/ModelPackage/Entities/EditorsChoiceResponse.swift
@@ -13,6 +13,15 @@ public struct EditorsChoiceResponse: Codable, Identifiable, Equatable {
 }
 
 public struct Items: Codable, Identifiable, Equatable {
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.backdrop_path = try container.decodeIfPresent(String.self, forKey: .backdrop_path)
+        self.id = try container.decode(Int.self, forKey: .id)
+        self.original_title = try container.decodeIfPresent(String.self, forKey: .original_title)
+        self.release_date = try container.decodeIfPresent(Date.self, forKey: .release_date)
+        self.poster_path = try container.decodeIfPresent(String.self, forKey: .poster_path)
+    }
+    
     public let backdrop_path: String?
     public let id: Int
     public let original_title: String?
